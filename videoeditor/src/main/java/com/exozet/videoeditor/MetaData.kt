6c746c48ae5242cd6b3248fa8881df954d93004a
Ömür Kumru <omur.kumru@exozet.com>
2019-05-09 13:49:21 +0200

    added processPath to metadata

--- videoeditor/src/main/java/com/exozet/videoeditor/MetaData.kt
+++ videoeditor/src/main/java/com/exozet/videoeditor/MetaData.kt
@@ -5,5 +5,6 @@ import android.net.Uri
 data class MetaData(
             var progress: String? = null,
             var message: String? = null,
-            var uri: Uri? = null
+            var uri: Uri? = null,
+            var processPathUri: Uri? = null
     )
